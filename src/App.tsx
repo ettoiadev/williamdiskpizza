b51094e94ee915e494e4e56894a2f89f85687406
ettoiadev <ettoiadev@gmail.com>
2025-07-22 23:00:11 -0300

    mudanca nome página sobre

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
-import About from "./pages/About";
+import About from "./pages/sobre";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
